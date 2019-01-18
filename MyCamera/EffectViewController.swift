c8782250481f12db58e8f29ac23c6159fd1c8bcb
YutaKanna <kannayuta0629@gmail.com>
2019-01-18 12:48:00 +0900

    新しいView Controller用のSwiftファイルを作成する

---
+++ MyCamera/EffectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EffectViewController.swift
+//  MyCamera
+//
+//  Created by 漢那優太 on 2019/01/18.
+//  Copyright © 2019 Yuta Kanna. All rights reserved.
+//
+
+import UIKit
+
+class EffectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
